f31bfa7728f86c10ff9abd38f093a6230424f9de
Diego Leal <diego.leal@brasas.com>
2022-05-23 20:03:42 -0300

    A

--- db/schema.rb
+++ db/schema.rb
@@ -64,14 +64,14 @@ ActiveRecord::Schema.define(version: 2022_05_21_154110) do
 
   create_table "products", force: :cascade do |t|
     t.string "sku"
-    t.string "localization"
-    t.float "price"
-    t.string "bike_condition"
-    t.boolean "available", default: true
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.string "photo_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "price_cents", default: 0, null: false
     t.bigint "user_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -91,5 +91,6 @@ ActiveRecord::Schema.define(version: 2022_05_21_154110) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
